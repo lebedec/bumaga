26e38caeafa954d036c78fc27bbcb3c99f686cdd
Ilya Lebedev <lebedev.games.mail@gmail.com>
2024-09-13 17:44:54 +0300

    fix array binding

--- src/view_model.rs
+++ src/view_model.rs
@@ -6,6 +6,7 @@ use log::error;
 
 use serde_json::{json, Value};
 use std::collections::{BTreeMap, HashMap, HashSet};
+use std::fmt::format;
 use std::time::Duration;
 use taffy::{NodeId, TaffyTree};
 
@@ -16,6 +17,7 @@ pub type Transformer = fn(Value) -> Value;
 pub struct ViewModel {
     bindings: Bindings,
     model: Value,
+    model_array_default: HashMap<String, Value>,
     pub(crate) transformers: HashMap<String, Transformer>,
     // state
     // pub(crate) focus: Option<NodeId>,
@@ -25,12 +27,34 @@ pub struct ViewModel {
 
 impl ViewModel {
     pub fn create(bindings: Bindings, model: Value) -> Self {
+        let mut model_array_default = HashMap::new();
+        Self::memorize_array_default("", &model, &mut model_array_default);
         Self {
             bindings,
             model,
+            model_array_default,
             transformers: default_transformers(),
             mouse: [0.0, 0.0],
             mouse_hovers: HashSet::new(),
+        }
+    }
+
+    fn memorize_array_default(
+        type_path: &str,
+        value: &Value,
+        default: &mut HashMap<String, Value>,
+    ) {
+        match value {
+            Value::Array(array) => {
+                default.insert(type_path.to_string(), array[0].clone());
+                Self::memorize_array_default(type_path, &array[0], default)
+            }
+            Value::Object(object) => {
+                for (key, value) in object {
+                    Self::memorize_array_default(&format!("{type_path}/{key}"), value, default)
+                }
+            }
+            _ => {}
         }
     }
 
@@ -40,9 +64,11 @@ impl ViewModel {
             &mut self.model,
             value,
             "",
+            "",
             &self.bindings,
             &mut reactions,
             &self.transformers,
+            &self.model_array_default,
         );
         reactions
     }
@@ -51,20 +77,35 @@ impl ViewModel {
         mut dst: &mut Value,
         src: &Value,
         path: &str,
+        arrays_path: &str,
         bindings: &Bindings,
         reactions: &mut Vec<Reaction>,
         transformers: &HashMap<String, Transformer>,
+        default: &HashMap<String, Value>,
     ) {
         match (&mut dst, src) {
             (Value::Array(current), Value::Array(next)) => {
-                if current != next {
-                    current.resize(next.len(), Value::Null);
-                    Self::react(path, src, bindings, reactions, transformers);
+                if current.len() != next.len() {
+                    if let Some(default) = default.get(arrays_path).cloned() {
+                        current.resize(next.len(), default);
+                        Self::react(path, src, bindings, reactions, transformers);
+                    } else {
+                        error!("unable to resize array {path} default not found");
+                    }
                 }
                 for (index, dst) in current.iter_mut().enumerate() {
                     let src = &next[index];
                     let path = format!("{path}/{index}");
-                    Self::bind_value(dst, src, &path, bindings, reactions, transformers);
+                    Self::bind_value(
+                        dst,
+                        src,
+                        &path,
+                        arrays_path,
+                        bindings,
+                        reactions,
+                        transformers,
+                        default,
+                    );
                 }
             }
             (Value::Array(_), _) => {
@@ -73,6 +114,7 @@ impl ViewModel {
             (Value::Object(object), Value::Object(src)) => {
                 for (key, dst) in object.iter_mut() {
                     let path = format!("{path}/{key}");
+                    let arrays_path = format!("{arrays_path}/{key}");
                     let src = match src.get(key) {
                         Some(src) => src,
                         None => {
@@ -80,13 +122,32 @@ impl ViewModel {
                             continue;
                         }
                     };
-                    Self::bind_value(dst, src, &path, bindings, reactions, transformers);
+                    Self::bind_value(
+                        dst,
+                        src,
+                        &path,
+                        &arrays_path,
+                        bindings,
+                        reactions,
+                        transformers,
+                        default,
+                    );
                 }
             }
             (Value::Object(object), Value::Null) => {
                 for (key, dst) in object.iter_mut() {
                     let path = format!("{path}/{key}");
-                    Self::bind_value(dst, &Value::Null, &path, bindings, reactions, transformers);
+                    let arrays_path = format!("{arrays_path}/{key}");
+                    Self::bind_value(
+                        dst,
+                        &Value::Null,
+                        &path,
+                        &arrays_path,
+                        bindings,
+                        reactions,
+                        transformers,
+                        default,
+                    );
                 }
             }
             (dst, src) => {
@@ -386,7 +447,7 @@ impl Binding {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Reaction {
     Type {
         node: NodeId,
@@ -515,4 +576,249 @@ fn default_transformers() -> HashMap<String, Transformer> {
     let mut transformers = HashMap::new();
     transformers.insert("duration".to_string(), duration as Transformer);
     transformers
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Binding, BindingParams, Reaction, ViewModel};
+    use serde_json::{json, Value};
+    use std::collections::BTreeMap;
+    use taffy::NodeId;
+
+    fn node(id: u64) -> NodeId {
+        NodeId::new(id)
+    }
+
+    fn repeat(node: u64, n: usize) -> Binding {
+        Binding {
+            params: BindingParams::Repeat(NodeId::new(node), 0, n),
+            pipe: vec![],
+        }
+    }
+
+    fn text(node: u64) -> Binding {
+        Binding {
+            params: BindingParams::Text(NodeId::new(node), 0),
+            pipe: vec![],
+        }
+    }
+
+    fn attr(node: u64, attr: &str) -> Binding {
+        Binding {
+            params: BindingParams::Attribute(NodeId::new(node), attr.to_string()),
+            pipe: vec![],
+        }
+    }
+
+    #[test]
+    pub fn test_rebind_simple_array_same_values_reduced_array() {
+        let model = json!({
+            "names": [null, null, null]
+        });
+        let [names, names_0, names_1, names_2] = [100, 200, 300, 400];
+        let bindings = BTreeMap::from([
+            ("/names".to_string(), vec![repeat(names, 3)]),
+            ("/names/0".to_string(), vec![text(names_0)]),
+            ("/names/1".to_string(), vec![text(names_1)]),
+            ("/names/2".to_string(), vec![text(names_2)]),
+        ]);
+        let mut view_model = ViewModel::create(bindings, model);
+        view_model.bind(&json!({ "names": ["Alice", "Boris"] }));
+
+        let reactions = view_model.bind(&json!({ "names": ["Alice"] }));
+
+        assert_eq!(
+            reactions,
+            vec![Reaction::Repeat {
+                parent: names.into(),
+                start: 0,
+                cursor: 1,
+                end: 3,
+            },]
+        );
+    }
+
+    #[test]
+    pub fn test_rebind_simple_array_new_values() {
+        let model = json!({
+            "names": [null, null, null]
+        });
+        let [names, names_0, names_1, names_2] = [100, 200, 300, 400];
+        let bindings = BTreeMap::from([
+            ("/names".to_string(), vec![repeat(names, 3)]),
+            ("/names/0".to_string(), vec![text(names_0)]),
+            ("/names/1".to_string(), vec![text(names_1)]),
+            ("/names/2".to_string(), vec![text(names_2)]),
+        ]);
+        let mut view_model = ViewModel::create(bindings, model);
+        view_model.bind(&json!({ "names": ["Alice", "Boris"] }));
+
+        let reactions = view_model.bind(&json!({ "names": ["Carol", "David"] }));
+
+        assert_eq!(
+            reactions,
+            vec![
+                Reaction::Type {
+                    node: names_0.into(),
+                    span: 0,
+                    text: "Carol".to_string(),
+                },
+                Reaction::Type {
+                    node: names_1.into(),
+                    span: 0,
+                    text: "David".to_string()
+                }
+            ]
+        );
+    }
+
+    #[test]
+    pub fn test_rebind_simple_array_new_values_increased_array() {
+        let model = json!({
+            "names": [null, null, null]
+        });
+        let [names, names_0, names_1, names_2] = [100, 200, 300, 400];
+        let bindings = BTreeMap::from([
+            ("/names".to_string(), vec![repeat(names, 3)]),
+            ("/names/0".to_string(), vec![text(names_0)]),
+            ("/names/1".to_string(), vec![text(names_1)]),
+            ("/names/2".to_string(), vec![text(names_2)]),
+        ]);
+        let mut view_model = ViewModel::create(bindings, model);
+        view_model.bind(&json!({ "names": ["Alice"] }));
+
+        let reactions = view_model.bind(&json!({ "names": ["Boris", "Carol"] }));
+
+        assert_eq!(
+            reactions,
+            vec![
+                Reaction::Repeat {
+                    parent: names.into(),
+                    start: 0,
+                    cursor: 2,
+                    end: 3,
+                },
+                Reaction::Type {
+                    node: names_0.into(),
+                    span: 0,
+                    text: "Boris".to_string(),
+                },
+                Reaction::Type {
+                    node: names_1.into(),
+                    span: 0,
+                    text: "Carol".to_string()
+                }
+            ]
+        );
+    }
+
+    #[test]
+    pub fn test_rebind_objects_array_same_values_reduced_array() {
+        let model = json!({
+            "items": [
+                {"id": null, "name": null},
+                {"id": null, "name": null},
+                {"id": null, "name": null}
+            ]
+        });
+        let [items, items_0, items_1, items_2] = [100, 200, 300, 400];
+        let bindings = BTreeMap::from([
+            ("/items".to_string(), vec![repeat(items, 3)]),
+            ("/items/0/name".to_string(), vec![text(items_0)]),
+            ("/items/0/id".to_string(), vec![attr(items_0, "id")]),
+            ("/items/1/name".to_string(), vec![text(items_1)]),
+            ("/items/1/id".to_string(), vec![attr(items_1, "id")]),
+            ("/items/2/name".to_string(), vec![text(items_2)]),
+            ("/items/2/id".to_string(), vec![attr(items_2, "id")]),
+        ]);
+        let mut view_model = ViewModel::create(bindings, model);
+        view_model.bind(&json!({
+            "items": [
+                {"id": 0, "name": "Alice"},
+                {"id": 1, "name": "Boris"}
+            ]
+        }));
+
+        let reactions = view_model.bind(&json!({
+            "items": [
+                {"id": 0, "name": "Alice"},
+            ]
+        }));
+
+        assert_eq!(
+            reactions,
+            vec![Reaction::Repeat {
+                parent: items.into(),
+                start: 0,
+                cursor: 1,
+                end: 3,
+            }]
+        );
+    }
+
+    #[test]
+    pub fn test_rebind_objects_array_new_values_increased_array() {
+        let model = json!({
+            "items": [
+                {"id": null, "name": null},
+                {"id": null, "name": null},
+                {"id": null, "name": null}
+            ]
+        });
+        let [items, items_0, items_1, items_2] = [100, 200, 300, 400];
+        let bindings = BTreeMap::from([
+            ("/items".to_string(), vec![repeat(items, 3)]),
+            ("/items/0/name".to_string(), vec![text(items_0)]),
+            ("/items/0/id".to_string(), vec![attr(items_0, "id")]),
+            ("/items/1/name".to_string(), vec![text(items_1)]),
+            ("/items/1/id".to_string(), vec![attr(items_1, "id")]),
+            ("/items/2/name".to_string(), vec![text(items_2)]),
+            ("/items/2/id".to_string(), vec![attr(items_2, "id")]),
+        ]);
+        let mut view_model = ViewModel::create(bindings, model);
+        view_model.bind(&json!({
+            "items": [
+                {"id": 0, "name": "Alice"}
+            ]
+        }));
+
+        let reactions = view_model.bind(&json!({
+            "items": [
+                {"id": 1, "name": "Boris"},
+                {"id": 2, "name": "Carol"},
+            ]
+        }));
+
+        assert_eq!(
+            reactions,
+            vec![
+                Reaction::Repeat {
+                    parent: items.into(),
+                    start: 0,
+                    cursor: 2,
+                    end: 3,
+                },
+                Reaction::Bind {
+                    node: items_0.into(),
+                    key: "id".to_string(),
+                    value: json!(1),
+                },
+                Reaction::Type {
+                    node: items_0.into(),
+                    span: 0,
+                    text: "Boris".to_string(),
+                },
+                Reaction::Bind {
+                    node: items_1.into(),
+                    key: "id".to_string(),
+                    value: json!(2),
+                },
+                Reaction::Type {
+                    node: items_1.into(),
+                    span: 0,
+                    text: "Carol".to_string()
+                }
+            ]
+        );
+    }
 }
