704dd8f5d8c136dafa38d0b91d5d180e24155152
Ilya Lebedev <lebedev.games.mail@gmail.com>
2024-09-25 18:02:25 +0300

    rewrite JavaScript stub

--- js/stub.js
+++ js/stub.js
@@ -2,13 +2,99 @@
  Implements simple Bumaga engine for development and prototyping of HTML view in browser.
  */
 
-let repeat_pattern = /\{(\w+)\}\*(\d+)/g;
-let attribute_pattern = /\[([\w-_]+)\]/g;
-let if_pattern = /\?/g;
-let else_pattern = /\!/g;
-let pipe_pattern = /\{([\w._]+)\}/;
-let pipe_pattern_global = /\{([\w._]+)\}/g;
-let callback_pattern = /\[([\w-_]+)\]~([\w-_]+)/;
+function traverse(node, value) {
+    if (node.nodeType !== 1) {
+        node.textContent = node.textContent.replaceAll(REGEX_PIPE_GLOBAL, (substring, key) => {
+            return getValue(value, key)
+        });
+        return;
+    }
+    if (node.getAttribute("repeated") === "true") {
+        return;
+    }
+    let parent = node.parentNode;
+    let newAttributes = [];
+    for (let attribute of node.attributes) {
+        if (attribute.name.startsWith("*")) {
+            let key = attribute.name.substring(1);
+            let [count, binder] = attribute.value.split(" ");
+            count = parseInt(count);
+            binder = parseBinder(binder);
+            parent.removeChild(node);
+            node.attributes.removeNamedItem(attribute.name);
+            for (let item of getValue(value, binder)) {
+                value[key] = item;
+                let child = node.cloneNode(true);
+                traverse(child, value);
+                child.setAttribute("repeated", "")
+                parent.appendChild(child);
+            }
+        }
+        if (attribute.name.startsWith("+")) {
+            let key = attribute.name.substring(1);
+            let binder = parseBinder(attribute.value);
+            value[key] = getValue(value, binder)
+        }
+        if (attribute.name.startsWith("^")) {
+            let handler = attribute.name.substring(1);
+            let [event, ...args] = attribute.value.split(" ");
+            let resolved = [];
+            for (let arg of args) {
+                if (arg === "this") {
+                    resolved.push("<this>");
+                } else {
+                    resolved.push(getValue(value, parseBinder(arg)))
+                }
+            }
+            node.addEventListener(handler.substring(2), () => {
+                console.log(event, resolved);
+            })
+        }
+        if (attribute.name.startsWith("?")) {
+            let binder = parseBinder(attribute.value);
+            if (!getValue(value, binder)) {
+                parent.removeChild(node);
+                return;
+            }
+        }
+        if (attribute.name.startsWith("!")) {
+            let binder = parseBinder(attribute.value);
+            if (getValue(value, binder)) {
+                parent.removeChild(node);
+                return;
+            }
+        }
+        if (attribute.name.startsWith("@")) {
+            let key = attribute.name.substring(1);
+            let attr = attribute.value.replaceAll(REGEX_PIPE_GLOBAL, (substring, path) => {
+                return getValue(value, path)
+            });
+            newAttributes.push([key, attr]);
+        }
+        if (attribute.name.startsWith("#")) {
+            let key = attribute.name.substring(1);
+            let binder = parseBinder(attribute.value);
+            if (getValue(value, binder)) {
+                newAttributes.push([key, ""]);
+            }
+        }
+    }
+    for (let setter of newAttributes) {
+        let [key, value] = setter;
+        node.setAttribute(key, value);
+    }
+    for (let child of node.childNodes) {
+        traverse(child, value);
+    }
+}
+
+
+let REGEX_PIPE_GLOBAL = /{([\w._]+)}/g;
+let REGEX_PIPE = /{([\w._]+)}/;
+
+function parseBinder(binder) {
+    return REGEX_PIPE.exec(binder)[1];
+}
 
 function getValue(value, path) {
     return getValueRecursive(value, path.split("."));
@@ -23,88 +109,6 @@ function getValueRecursive(value, path) {
     }
 }
 
-function traverse(node, value) {
-    if (node.nodeType !== 1) {
-        node.textContent = node.textContent.replaceAll(pipe_pattern_global, (substring, key) => {
-            return getValue(value, key)
-        });
-        return;
-    }
-
-    if (node.getAttribute("rendered") === "true") {
-        return;
-    }
-
-    let parent = node.parentNode;
-    for (let attribute of node.attributes) {
-        if (if_pattern.exec(attribute.name) != null) {
-            let getter = pipe_pattern.exec(attribute.value)[1];
-            let test = getValue(value, getter);
-            if (!test) {
-                parent.removeChild(node);
-                return;
-            }
-        }
-        if (else_pattern.exec(attribute.name) != null) {
-            let getter = pipe_pattern.exec(attribute.value)[1];
-            let test = getValue(value, getter);
-            if (!!test) {
-                parent.removeChild(node);
-                return;
-            }
-        }
-
-        let repeat = repeat_pattern.exec(attribute.name);
-        if (repeat != null) {
-            let getter = repeat[1];
-            let count = repeat[2];
-            let items = pipe_pattern.exec(attribute.value)[1];
-            parent.removeChild(node);
-            node.attributes.removeNamedItem(attribute.name);
-            for (let item of getValue(value, items)) {
-                value[getter] = item;
-                let child = node.cloneNode(true);
-                parent.appendChild(child);
-                traverse(child, value);
-                child.setAttribute("rendered", "true")
-            }
-            // todo: count
-            // todo: override
-            delete value[getter];
-            return;
-        }
-    }
-
-    let attributes = [];
-    for (let attribute of node.attributes) {
-        let attr = attribute_pattern.exec(attribute.name);
-        if (attr != null) {
-            let name = attr[1];
-            let getter = pipe_pattern.exec(attribute.value)[1];
-            attributes.push([name, getValue(value, getter)]);
-        }
-        let callback = callback_pattern.exec(attribute.name);
-        if (callback != null) {
-            let event = callback[1];
-            let handler = callback[2];
-            let expr = attribute.value;
-            attributes.push([event, `emit("${handler}${expr}")`])
-        }
-    }
-    for (let setter of attributes) {
-        let [key, value] = setter;
-        node.setAttribute(key, value);
-    }
-
-    for (let child of node.childNodes) {
-        traverse(child, value);
-    }
-}
-
 window.onload = () => {
     traverse(document.body, VALUE)
-}
-
-function emit(...args) {
-    console.log('output', args)
 }
